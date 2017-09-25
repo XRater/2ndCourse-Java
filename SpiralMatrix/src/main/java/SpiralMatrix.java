b72824716de886dd7b562794590237cd5e533951
Kirakosian Aleksander <kair1998@gmail.com>
2017-09-26 01:07:52 +0300

    SpiralMatrix gradle JUnit5

--- SpiralMatrix/src/main/java/SpiralMatrix.java
+++ SpiralMatrix/src/main/java/SpiralMatrix.java
@@ -1,9 +1,24 @@
 public class SpiralMatrix {
 
-    int[][] data;
+    private int[][] data;
+    private final int N;
 
     SpiralMatrix(int[][] data) {
-        this.data = data;
+        N = data.length;
+
+        for (int[] aData : data) {
+            if (aData.length != N) {
+                throw new IllegalArgumentException();
+            }
+        }
+        this.data = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                this.data[i][j] = data[j][i];
+            }
+        }
+
     }
 
 }
---
+++ SpiralMatrix/src/test/java/SpiralMatrixTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class SpiralMatrixTest {
+
+    @Test
+    public void foo() {
+        assertThrows(Exception.class, () -> {throw new Exception();});
+        assertEquals(1, 1);
+    }
+}
