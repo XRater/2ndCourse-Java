1acd1d46ae995f63fa4772ce2fe0f6e63a81b1b1
Kirakosian Aleksander <kair1998@gmail.com>
2017-09-27 18:00:20 +0300

    SpiralMatrix SpiralPrint with docs and tests

--- SpiralMatrix/src/main/java/StrangeMatrix.java
+++ SpiralMatrix/src/main/java/StrangeMatrix.java
@@ -3,7 +3,22 @@ import java.util.Comparator;
 
 public class StrangeMatrix {
 
-    public static void spiralPrint(int[][] data) {
+    /**
+     * This method returns two-dimensional array in String form.
+     * Elements will be added in a spiral sequence. Comma is used as delimiter.
+     * For example
+     * <p> 9 2 3
+     * <p> 8 1 4
+     * <p> 7 6 5
+     * <p>
+     * returns [1, 2, 3, 4, 5, 6, 7, 8, 9]
+     * <p>
+     * Array must be completed quad matrix N x N with odd N.
+     * All columns must have same odd sizes. If they do not, IllegalArgumentException will be thrown.
+     * @param data two-dimensional array to work with
+     * @return String representation of your array
+     */
+    public static String spiralPrint(int[][] data) {
         if (data == null)
             throw new IllegalArgumentException();
 
@@ -16,8 +31,18 @@ public class StrangeMatrix {
         if (!isMatrixRectangular(data, n)) {
             throw new IllegalArgumentException();
         }
-    }
 
+        StringBuilder sb = new StringBuilder();
+        int center = n / 2;
+        sb.append(data[center][center]);
+        for (int step = 1; step <= center; step++) {
+            printLine(data, sb, center - step, center - step + 1, center - step, center + step);
+            printLine(data, sb, center - step + 1, center + step, center + step, center + step);
+            printLine(data, sb, center + step, center + step - 1, center + step, center - step);
+            printLine(data, sb, center + step - 1, center - step, center - step, center - step);
+        }
+        return sb.toString();
+    }
 
     /**
      * Sorts columns of given two-dimensional array by the first row.
@@ -77,6 +102,33 @@ public class StrangeMatrix {
         return true;
     }
 
+    /**
+     * This method appends line of data elements to the end of StringBuilder.
+     * It starts with (beginX, beginY) point and ends with (endX, endY) point.
+     * <p>
+     * Lane must be horizontal or vertical. If it is not, IllegalArgumentException will be thrown.
+     */
+    private static void printLine(int[][] data, StringBuilder sb, int beginX, int beginY, int endX, int endY) {
+        if (beginX != endX && beginY != endY)
+            throw new IllegalArgumentException();
+
+        if (beginX == endX) {
+            int dy = sign(endY - beginY);
+            for (int y = beginY; y != endY + dy; y += dy) {
+                sb.append(", ").append(data[beginX][y]);
+            }
+        } else {
+            int dx = sign(endX - beginX);
+            for (int x = beginX; x != endX + dx; x += dx) {
+                sb.append(", ").append(data[x][beginY]);
+            }
+        }
+    }
+
+    private static int sign(int x) {
+        return Integer.compare(x, 0);
+    }
+
     private static void copyRotateTwoDimensionalArray(int[][] source, int[][] dest, int n, int m) {
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
@@ -84,7 +136,6 @@ public class StrangeMatrix {
             }
         }
     }
-
 }
 
 
--- SpiralMatrix/src/test/java/StrangeMatrixTest.java
+++ SpiralMatrix/src/test/java/StrangeMatrixTest.java
@@ -96,4 +96,69 @@ public class StrangeMatrixTest {
         assertThrows(IllegalArgumentException.class,
                 () -> StrangeMatrix.sortByColumns(new int[][]{null}));
     }
+
+    @Test
+    public void testSpiralPrintRegular() {
+        int[][] data = {{1}};
+        assertEquals("1", StrangeMatrix.spiralPrint(data));
+
+        data = new int[][]{ {9, 2, 3},
+                            {8, 1, 4},
+                            {7, 6, 5}};
+        assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9", StrangeMatrix.spiralPrint(data));
+
+        data = new int[][]{ {25, 10, 11, 12, 13},
+                            {24, 9, 2, 3, 14},
+                            {23, 8, 1, 4, 15},
+                            {22, 7, 6, 5, 16},
+                            {21, 20, 19, 18, 17}};
+        assertEquals("1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25",
+                StrangeMatrix.spiralPrint(data));
+     }
+
+    @Test
+    public void testSpiralPrintExceptions() {
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(null));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{1, 2}, {}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{}, {1, 2}, {3, 4}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{}, {}, {}, {}, {}, {3, 4}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{1, 2}, {1, 2}, {3, 4, 5}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{1, 2}, null}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{null, null}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{1, 2, 3}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{1, 2}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{1, 2, 3}, {4, 5, 6}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{1, 2}, {3, 4}}));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> StrangeMatrix.spiralPrint(new int[][]{{0, 0, 0, 0}, {0, 0, 0, 0},
+                                                            {0, 0, 0, 0}, {0, 0, 0, 0}}));
+    }
 }
