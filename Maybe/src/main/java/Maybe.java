f019b779c008145a9378ba9103a586b410ea6ca2
Kirakosian Aleksander <kair1998@gmail.com>
2017-10-04 18:05:06 +0300

    Maybe class base functions & gradle

---
+++ Maybe/src/main/java/Maybe.java
@@ -0,0 +1,48 @@
+import exceptions.NoElementInMaybeException;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.function.Function;
+
+public class Maybe<T> {
+
+    @Nullable
+    private final T value;
+    private final boolean empty;
+
+    public static <T> Maybe<T> just(T t) {
+        return new Maybe<>(t);
+    }
+
+    public static <T> Maybe<T> nothing() {
+        return new Maybe<>();
+    }
+
+    private Maybe() {
+        value = null;
+        empty = true;
+    }
+
+    private Maybe(@Nullable T t) {
+        value = t;
+        empty = false;
+    }
+
+    @Nullable
+    public T get() throws NoElementInMaybeException {
+        if (empty)
+            throw new NoElementInMaybeException();
+        return value;
+    }
+
+    public boolean isPresent() {
+        return !empty;
+    }
+
+    @NotNull
+    public <U> Maybe<U> map(@NotNull Function<? super T, U> mapper) {
+        if (empty)
+            return new Maybe<>();
+        return new Maybe<>(mapper.apply(value));
+    }
+}
---
+++ Maybe/src/main/java/exceptions/NoElementInMaybeException.java
@@ -0,0 +1,5 @@
+package exceptions;
+
+public class NoElementInMaybeException extends Exception {
+
+}
