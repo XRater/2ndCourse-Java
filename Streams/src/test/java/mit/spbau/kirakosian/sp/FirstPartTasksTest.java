761a7094701be28729c54597ff6d3bf5639ca06f
Kirakosian Aleksander <kair1998@gmail.com>
2017-12-05 16:06:38 +0300

    compiled classes

--- Streams/src/test/java/mit/spbau/kirakosian/sp/FirstPartTasksTest.java
+++ Streams/src/test/java/mit/spbau/kirakosian/sp/FirstPartTasksTest.java
@@ -5,7 +5,9 @@ import com.google.common.collect.ImmutableMap;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static mit.spbau.kirakosian.sp.FirstPartTasks.*;
