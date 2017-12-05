2b78744fa885c88db5f2542014ab925e23373051
Kirakosian Aleksander <kair1998@gmail.com>
2017-12-05 15:52:22 +0300

    classes

---
+++ Streams/src/test/java/mit/spbau/kirakosian/sp/SecondPartTasksTest.java
@@ -0,0 +1,28 @@
+package mit.spbau.kirakosian.sp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SecondPartTasksTest {
+
+    @Test
+    public void testFindQuotes() {
+//        fail();
+    }
+
+    @Test
+    public void testPiDividedBy4() {
+//        fail();
+    }
+
+    @Test
+    public void testFindPrinter() {
+//        fail();
+    }
+
+    @Test
+    public void testCalculateGlobalOrder() {
+//        fail();
+    }
+}
